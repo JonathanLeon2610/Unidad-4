05733517864ecc750e29883d76cac71f3f91bece
JonathanLeon2610 <111078357+JonathanLeon2610@users.noreply.github.com>
2022-10-02 21:46:37 -0500

    Add files via upload

---
+++ Crud-Plantilla-add_img/layouts/sidebar.template.php
@@ -0,0 +1,16 @@
+<div class="row">
+            <div class="col-lg-2 d-none d-sm-block ">
+                <aside class="bg-light">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="index.php">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link disabled">Disabled</a>
+                        </li>
+                      </ul>
+                </aside>
+            </div>
